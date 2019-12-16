75f7bf637614a18fa063d73da67b351422cc1b36
Jorge Mancero <jman0829@gmail.com>
2019-12-15 22:24:49 -0500

    public folder updated

--- config/connection.js
+++ config/connection.js
@@ -9,12 +9,12 @@ connection = mysql.createConnection({
     database: "burgers_db"
 });
 
-connection,connect(function(err) {
+connection.connect(function(err) {
     if (err) {
-        console.error("error connecting: " + err.stack);
-        return
+      console.error("error connecting: " + err.stack);
+      return;
     }
-    console.log("connected as id " = connection.threadId);
-});
+    console.log("connected as id " + connection.threadId);
+  });
 
 module.exports = connection;
